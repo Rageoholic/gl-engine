56555123079dfa2bc9dee2848b9f8eb0191c866f
Hersh Krishna <hershkrishna@protonmail.ch>
2018-11-26 20:37:27 -0600

    Turned on the depth buffer

--- linux-platform.c
+++ linux-platform.c
@@ -7,6 +7,9 @@
 #define WIDTH 1280
 #define HEIGHT 720
 
+#define VERT_SHADER_PATH "shaders/basic-render.vert"
+#define FRAG_SHADER_PATH "shaders/basic-render.frag"
+
 typedef struct Vertex
 {
     Vec3f pos;
@@ -14,12 +17,20 @@ typedef struct Vertex
 } Vertex;
 
 local Vertex vertices[] = {
+    {{.5, .5, .5}, {1, 0, 0}},
+    {{-.5, .5, .5}, {0, 1, 0}},
+    {{.5, -.5, .5}, {0, 0, 1}},
+    {{.5, -.5, .5}, {0, 0, 1}},
+    {{-.5, .5, .5}, {0, 1, 0}},
+    {{-.5, -.5, .5}, {1, 1, 1}},
     {{.5, .5, 0}, {1, 0, 0}},
     {{-.5, .5, 0}, {0, 1, 0}},
     {{.5, -.5, 0}, {0, 0, 1}},
     {{.5, -.5, 0}, {0, 0, 1}},
     {{-.5, .5, 0}, {0, 1, 0}},
-    {{-.5, -.5, 0}, {1, 1, 1}}};
+    {{-.5, -.5, 0}, {1, 1, 1}}
+
+};
 
 f32 SDLGetAspectRatio(SDL_Window *win)
 {
@@ -41,8 +52,10 @@ int main(int argc, char **argv)
     SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 4);
     SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 5);
     SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
+
+    /* TODO: properly get multisample values */
     SDL_GL_SetAttribute(SDL_GL_MULTISAMPLEBUFFERS, 1);
-    SDL_GL_SetAttribute(SDL_GL_MULTISAMPLESAMPLES, 4);
+    SDL_GL_SetAttribute(SDL_GL_MULTISAMPLESAMPLES, 16);
 
     SDL_Window *win = SDL_CreateWindow("Title", 0, 0, WIDTH, HEIGHT, SDL_WINDOW_OPENGL);
 
@@ -50,11 +63,12 @@ int main(int argc, char **argv)
 
     SDL_GL_MakeCurrent(win, c);
 
-    SDL_GL_SetSwapInterval(0);
+    if (SDL_GL_SetSwapInterval(-1) == -1)
+    {
+        SDL_GL_SetSwapInterval(1);
+    }
 
     INVARIANT(gladLoadGLLoader((GLADloadproc)SDL_GL_GetProcAddress), "Could not load GL");
-
-    bool running = true;
 
     u32 vertexBuffer;
 
@@ -67,22 +81,30 @@ int main(int argc, char **argv)
 
     glBindVertexArray(vertexArrayObject);
 
+    /* Vertex position in worldspace */
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void *)offsetof(Vertex, pos));
     glEnableVertexAttribArray(0);
+
+    /* Vertex color */
     glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void *)offsetof(Vertex, c));
     glEnableVertexAttribArray(1);
 
     Mat4f proj = CreatePerspectiveMat4f(DegToRad(45), SDLGetAspectRatio(win), .1, 10);
     Mat4f view = CalcLookAtMat4f(vec3f(3, 3, 3), vec3f(0, 0, 0), vec3f(0, 0, 1));
 
-    ShaderProg s = CreateShaderProg("shaders/basic-render.vert", "shaders/basic-render.frag");
+    ShaderProg s = CreateShaderProg(VERT_SHADER_PATH, FRAG_SHADER_PATH);
 
     UseShaderProg(s);
     SetUniformMat4fShaderProg(s, "proj", &proj);
     SetUniformMat4fShaderProg(s, "view", &view);
 
-    f32 totalTime = 0;
+    glEnable(GL_MULTISAMPLE);
+    glEnable(GL_DEPTH_TEST);
 
+    glClearColor(.1, .1, .1, 1);
+
+    bool running = true;
+    f32 totalTime = 0;
     ufast32 lastTime = SDL_GetTicks();
 
     while (running)
@@ -108,15 +130,19 @@ int main(int argc, char **argv)
             }
         }
         /* update */
-        Mat4f model = RotateMat4f(&IdMat4f, totalTime * DegToRad(40), vec3f(0, 0, 1));
+        Mat4f model = RotateMat4f(&IdMat4f, totalTime * DegToRad(20), vec3f(0, 0, 1));
 
         /* Render */
-        glClearColor(1, .2, .8, 1);
-        SetUniformMat4fShaderProg(s, "model", &model);
-        glBindVertexArray(vertexArrayObject);
-        glClear(GL_COLOR_BUFFER_BIT);
-        UseShaderProg(s);
-        glDrawArrays(GL_TRIANGLES, 0, countof(vertices));
+        {
+            glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+            /* Set relevant state */
+            SetUniformMat4fShaderProg(s, "model", &model);
+
+            glBindVertexArray(vertexArrayObject);
+            UseShaderProg(s);
+            glDrawArrays(GL_TRIANGLES, 0, countof(vertices));
+        }
 
         /* End of frame housekeeping */
         SDL_GL_SwapWindow(win);
